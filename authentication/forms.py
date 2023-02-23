fae2790d3ef22df4c3b645d3033ca296216a2e39
Thomas Creusot <th.creusot@gmail.com>
2023-02-23 17:12:48 +0100

    creation of signupForm in authentication/forms.py, signup view and signup.html template

--- authentication/forms.py
+++ authentication/forms.py
@@ -8,3 +8,12 @@ class LoginForm(forms.Form):
 
     username = forms.CharField(max_length=100, label='Username')
     password = forms.CharField(max_length=50, widget=forms.PasswordInput, label='Password')
+
+
+class SignupForm(UserCreationForm):
+    """Represents a sign up form"""
+
+    class Meta(UserCreationForm.Meta):
+        # get_user_model() allows to get the User model without importing it directly
+        model = get_user_model()
+        fields = ('username', 'email', 'birth_date')
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from . import forms
@@ -24,3 +25,15 @@ def login_page(request):
 def logout_user(request):
     logout(request)
     return redirect('index')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
