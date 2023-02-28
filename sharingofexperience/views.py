19f03a61469829098813624cdc117733274911ca
Thomas Creusot <th.creusot@gmail.com>
2023-02-28 12:14:24 +0100

    Creation of functions sharing_of_experience_already_created_for_an_age() + factorisation with functions experienced_age_to_be_created_lower_than_user_age_boolean() and sharing_of_experience_of_an_user_at_a_given_age_id() in views.py

--- sharingofexperience/views.py
+++ sharingofexperience/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from datetime import date
 from .models import SharingOfExperience
 from sharingofexperience.forms import SharingOfExperienceFormCreate
@@ -10,6 +11,25 @@ LOWER_LIMIT_AGE_TO_BE_SHARED = 10 #  years old
 def age_calculation(birth_date):
     today = date.today()
     return today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+
+def sharing_of_experience_already_created_for_an_age(request, experienced_age):
+    user_sharing_of_experiences = SharingOfExperience.objects.filter(user_id_id = request.user.id)
+    age_already_filled_boolean = experienced_age in [user_sharing_of_experience.experienced_age for user_sharing_of_experience in user_sharing_of_experiences]
+    return age_already_filled_boolean
+
+
+def experienced_age_to_be_created_lower_than_user_age_boolean(experienced_age, request):
+    user_age = age_calculation(request.user.birth_date)    
+    return experienced_age < user_age
+
+
+def sharing_of_experience_of_an_user_at_a_given_age_id(request, experienced_age):
+    sharing_of_experience_already_created = SharingOfExperience.objects.filter(
+        Q(user_id_id = request.user.id) & Q(experienced_age=experienced_age)
+    )
+    sharing_of_experience_already_created_id = sharing_of_experience_already_created[0].id
+    return sharing_of_experience_already_created_id
 
 
 def index(request):
@@ -44,23 +64,31 @@ def sharing_an_experience_create(request, experienced_age):
 
             # New instance but without saving in database
             sharing_of_experience = form.save(commit=False)
-            # set the connected user to the user before saving the model
+            # set the connected user to the user and other data before saving the model
             sharing_of_experience.user_id_id = request.user.id
             sharing_of_experience.moderator_validation = "NOP"  # NOP = "Not processed yet"
             sharing_of_experience.likes = {"likes": {}}
             sharing_of_experience.experienced_age = experienced_age
-
             # Save
             sharing_of_experience.save()
             return redirect('sharing_experiences_menu')
     else:
         form = SharingOfExperienceFormCreate()
 
-    user_age = age_calculation(request.user.birth_date)
-    if experienced_age < user_age:
-        return render(request, 'sharingofexperience/sharing_an_experience_create.html', {'form': form})
-    else : 
+
+    age_already_filled_boolean = sharing_of_experience_already_created_for_an_age(request, experienced_age)
+    experienced_age_to_be_created_low_enough_boolean = experienced_age_to_be_created_lower_than_user_age_boolean(experienced_age, request)
+
+    if not experienced_age_to_be_created_low_enough_boolean:
         return render(request, 'sharingofexperience/be_patient.html')
+    else : 
+        if age_already_filled_boolean:
+            sharing_of_experience_already_created_id=sharing_of_experience_of_an_user_at_a_given_age_id(request, experienced_age)
+            return redirect('sharing_an_experience_update', sharing_of_experience_already_created_id)        
+        else: 
+            return render(request, 'sharingofexperience/sharing_an_experience_create.html', {'form': form})
+
+
 
 @login_required
 def sharing_an_experience_update(request, sharing_of_experience_id):
