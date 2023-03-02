f267d178504c9a978b1e81b1e943b3d17510ed92
Thomas Creusot <th.creusot@gmail.com>
2023-03-02 11:44:59 +0100

    Creation of user_age_plus_minus_range_generation() function : display of sharing_of_experience with one year gap

--- sharingofexperience/views.py
+++ sharingofexperience/views.py
@@ -6,8 +6,8 @@ from .models import SharingOfExperience
 from sharingofexperience.forms import SharingOfExperienceFormCreate
 
 
-LOWER_LIMIT_AGE_TO_BE_SHARED = 10 #  years old
-
+LOWER_LIMIT_AGE_TO_BE_SHARED = 10  # years old
+GAP_OF_YEARS_FROM_USER_AGE_FOR_DISPLAYING_EXPERIENCES = 1  # years old
 
 def age_calculation(birth_date):
     today = date.today()
@@ -21,7 +21,7 @@ def sharing_of_experience_already_created_for_an_age(request, experienced_age):
 
 
 def experienced_age_to_be_created_lower_than_user_age_boolean(experienced_age, request):
-    user_age = age_calculation(request.user.birth_date)    
+    user_age = age_calculation(request.user.birth_date)
     return experienced_age < user_age
 
 
@@ -56,6 +56,15 @@ def sharing_of_experience_found(sharing_of_experience_search):
 
 def index(request):
     return render(request, 'sharingofexperience/index.html')
+
+
+def user_age_plus_minus_range_generation(user_age):
+    GAP = GAP_OF_YEARS_FROM_USER_AGE_FOR_DISPLAYING_EXPERIENCES
+    plus_minus_range = 2 * GAP + 1
+    user_age_plus_minus_range = []
+    for i in range(plus_minus_range):
+        user_age_plus_minus_range.append(user_age - GAP + i)
+    return user_age_plus_minus_range
 
 
 @login_required
@@ -140,12 +149,19 @@ def sharing_an_experience_update(request, sharing_of_experience_id):
         else:
             return render(request, 'sharingofexperience/not_your_experience.html')
 
+
 @login_required
 def learning_from_others(request):
+    user_age = age_calculation(request.user.birth_date)
+
+    user_age_plus_minus_range = user_age_plus_minus_range_generation(user_age)
+
     sharing_of_experiences_from_others = SharingOfExperience.objects.filter(
-    ~Q(user_id_id = request.user.id)
-        )
+        ~Q(user_id_id = request.user.id) & Q(experienced_age__in=user_age_plus_minus_range)
+    )
+
     context = {
         'sharing_of_experiences':sharing_of_experiences_from_others
     }
+
     return render(request, 'sharingofexperience/learning_from_others.html', context=context)
