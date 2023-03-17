baecf3f53d73d4690885b685d5f6dba33762c4b7
Thomas Creusot <th.creusot@gmail.com>
2023-03-17 12:01:13 +0100

    test_index() done

--- Partdage/Tests/sharingofexperience/test_unit_views.py
+++ Partdage/Tests/sharingofexperience/test_unit_views.py
@@ -1,12 +1,76 @@
-class TestABCViews:
+from django.urls import reverse
+from django.test import Client
+from pytest_django.asserts import assertTemplateUsed
+
+
+"""
+Utiliser Mocks pour de l'unitaire
+
+
+Organisation tests en classes
+Pourquoi utiliser une classe de test ?  Pour :
+-regrouper l’ensemble des tests d’une classe ;
+-définir un environnement fixe pour l'exécution des méthodes de test ;
+-éliminer la duplication du code commun pour tous les cas de test ;
+-faciliter le refactoring des tests. 
+
+Cette classe de test portera le nom de la classe à tester, précédé par Test
+Perso je partirai bien sur une classe par view !
+et chaque méthode de cette classe serait un scenario : propre.
+
+
+gestion d’une bdd temporaire avec décorateur @pytest.mark.django_db
+--> remplace les mocks ? Absolument pas car ce decorateur va juste faire en sorte que la base de donnees soit dans un etat correct ca ne la remplace pas
+
+Tester URL : 
+voir cours
+
+Tester vues: 
+voir cours
+
+Tester modèles: 
+voir cours
+
+
+Client django pour lancer une requete
+Le framework Django fournit un client de test afin de pouvoir simuler les requêtes HTTP sur notre application
+from django.test import Client
+client = Client()
+
+Faites une requête GET
+client = Client()
+client.get('/path/', {'key1': 'data1', 'key2': 'data2’})
+Equivalent à une requête GET sur l’URL :/path/?key1=data1&key2=data2.
+
+Faites une requête POST
+client = Client()
+client.post('/path/', {'key1': 'data1', 'key2': 'data2’})
+Equivalent à une requête POST sur l’URL/path/et avec une en-tête HTTP contenant key1=data1&key2=data2.
+
+
+class TestIndexView:
     def setup_method(self, method):
-        """ fonction appelée lors du lancement d'un test unitaire faisant partie d'une classe """
+        #fonction appelée lors du lancement d'un test unitaire faisant partie d'une classe
 
     def test_signup_view(self):
         assert 1 == 1
 
-    def test_testTwoName (self):
-        assert 2 == 2
 
     def teardown_method(self, method):
-        """ fonction appelée à la fin d'un test unitaire faisant partie d'une classe """
+        #fonction appelée à la fin d'un test unitaire faisant partie d'une classe
+"""
+
+
+class TestIndexView:
+
+    def test_index(self):
+        client = Client()
+        path = reverse('index')
+        response = client.get(path)
+        content = response.content.decode()
+
+        expected_content = "<h1>Welcome !</h1>"
+
+        assert response.status_code == 200
+        assertTemplateUsed(response, "sharingofexperience/index.html")
+        assert content.find(expected_content) != -1
--- Partdage/Tests/sharingofexperience/test_urls.py
+++ Partdage/Tests/sharingofexperience/test_urls.py
@@ -14,6 +14,7 @@ class TestSharingofExperienceUrls:
         assert resolve(url).view_name == 'sharing_an_experience_create'
         assert resolve(url).func == sharing_an_experience_create
 
+
     def test_sharing_an_experience_update(self):
         """Testing if the 'sharing_an_experience_update' (name) route is mapping to sharing_an_experience_update (view)"""
 
@@ -21,6 +22,7 @@ class TestSharingofExperienceUrls:
         assert url == "/sharing_an_experience_update/1/"
         assert resolve(url).view_name == 'sharing_an_experience_update'
         assert resolve(url).func == sharing_an_experience_update
+
 
     def test_like_a_sharing_of_experience(self):
         """Testing if the 'like_a_sharing_of_experience' (name) route is mapping to like_a_sharing_of_experience (view)"""
@@ -30,6 +32,7 @@ class TestSharingofExperienceUrls:
         assert resolve(url).view_name == 'like_a_sharing_of_experience'
         assert resolve(url).func == like_a_sharing_of_experience
 
+
     def test_spend_credits(self):
         """Testing if the 'spend_credits' (name) route is mapping to spend_credits (view)"""
 
@@ -37,4 +40,3 @@ class TestSharingofExperienceUrls:
         assert url == "/spend_credits/past_sharings/"
         assert resolve(url).view_name == 'spend_credits'
         assert resolve(url).func == spend_credits
-        print(url)
