259c9cb79128b070aa2e59193788711e40e2ed37
Thomas Creusot <th.creusot@gmail.com>
2023-03-17 11:25:57 +0100

    sharing_an_experience_create url test done

--- Partdage/Tests/sharingofexperience/test_unit_views.py
+++ Partdage/Tests/sharingofexperience/test_unit_views.py
@@ -1,2 +1,12 @@
-def test_a():
-    assert 1 == 1
+class TestABCViews:
+    def setup_method(self, method):
+        """ fonction appelée lors du lancement d'un test unitaire faisant partie d'une classe """
+
+    def test_signup_view(self):
+        assert 1 == 1
+
+    def test_testTwoName (self):
+        assert 2 == 2
+
+    def teardown_method(self, method):
+        """ fonction appelée à la fin d'un test unitaire faisant partie d'une classe """
---
+++ Partdage/Tests/sharingofexperience/test_urls.py
@@ -0,0 +1,35 @@
+from django.urls import reverse, resolve
+from sharingofexperience.views import sharing_an_experience_create, sharing_an_experience_update, like_a_sharing_of_experience, spend_credits
+
+
+class TestSharingofExperienceUrls:
+
+    def test_sharing_an_experience_create(self):
+        """Testing if the 'sharing_an_experience_create' (name) route is mapping to sharing_an_experience_create (view)"""
+
+        url = reverse('sharing_an_experience_create', args=[1])
+
+        # Vérifie si l’URL est correcte
+        assert url == "/sharing_an_experience_create/1/"
+        # # Vérifie si le nom de la vue est correct et que l’URL correspond bien au nom de la vue.
+        assert resolve(url).view_name == 'sharing_an_experience_create'
+        assert resolve(url).func == sharing_an_experience_create
+
+
+    def test_sharing_an_experience_update(self):
+        assert 1 == 1
+
+    def test_like_a_sharing_of_experience(self):
+        assert 1 == 1
+
+    def test_spend_credits(self):
+        assert 1 == 1
+
+
+
+"""
+    path('sharing_an_experience_create/<int:experienced_age>/', sharingofexperience.views.sharing_an_experience_create, name='sharing_an_experience_create'),
+    path('sharing_an_experience_update/<int:sharing_of_experience_id>/', sharingofexperience.views.sharing_an_experience_update, name='sharing_an_experience_update'),
+    path('like_a_sharing_of_experience/<int:id_sharing_of_experience_to_be_liked>/', sharingofexperience.views.like_a_sharing_of_experience, name='like_a_sharing_of_experience'),
+    path('spend_credits/<slug:past_or_future_sharings>/', sharingofexperience.views.spend_credits, name='spend_credits'),
+"""
