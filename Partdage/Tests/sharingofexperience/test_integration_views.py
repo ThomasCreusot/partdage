7336c3a8bf3a89065b51bd0c7a334a008714269d
Thomas Creusot <th.creusot@gmail.com>
2023-03-29 11:03:43 +0200

    amelioration and ending TestHomeView tests

--- Partdage/Tests/sharingofexperience/test_integration_views.py
+++ Partdage/Tests/sharingofexperience/test_integration_views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytest
 
 from django.urls import reverse
@@ -9,6 +10,8 @@ from pytest_django.asserts import assertTemplateUsed
 from authentication.models import User
 from sharingofexperience.models import ProfileModelSharingOfExperiencesUserHasAccess, SharingOfExperience
 
+from sharingofexperience.views import ACCESS_TO_SHARINGS_MINIMUM_NUMBER, GAP_OF_YEARS_FROM_USER_AGE_FOR_DISPLAYING_EXPERIENCES
+from sharingofexperience.views import age_calculation
 
 """
 Organisation tests en classes
@@ -68,7 +71,8 @@ class TestIndexView:
 class TestAuthenticationViews():
     @pytest.mark.django_db
     def test_signup_page_and_login_page_right_credentials(self):
-        """Tests if a user can sign-up and log-in"""
+        """Tests if a user can sign-up and log-in + creation of a ProfileModel object during first
+        connection"""
 
         client = Client()
 
@@ -86,16 +90,15 @@ class TestAuthenticationViews():
         assert ProfileModelSharingOfExperiencesUserHasAccess.objects.all().count() == 0
 
         response = client.post(reverse('login'), {'username': 'test_user_signup', 'password': 'TestUserPassword'})
-
         assert response.status_code == 302
         assert response.url == reverse('home')
+        # in views.py : try/except ProfileModelSharingOfExperiencesUserHasAccess.
+        # sharing_of_experiences_user_has_access = {"dictionary initialisation": 1}
+        assert ProfileModelSharingOfExperiencesUserHasAccess.objects.all().count() == 1
 
         # Test that the user is well authenticated -> to be studied.
         # user = auth.get_user(client)
         # assert user.is_authenticated
-
-        # ProfileModelSharingOfExperiencesUserHasAccess.sharing_of_experiences_user_has_access = {"dictionary initialisation": 1}
-        assert ProfileModelSharingOfExperiencesUserHasAccess.objects.all().count() == 1
 
 
     @pytest.mark.django_db
@@ -116,8 +119,8 @@ class TestAuthenticationViews():
 
         temp_user = client.post(reverse('signup'), credentials)
         response = client.post(reverse('login'), {'username': 'test_user_signup', 'password': 'WrongPassword'})
-
         assert response.status_code == 200
+
         content = response.content.decode()
         assert content.find("Invalid credentials.") != -1 
 
@@ -141,45 +144,46 @@ class TestIndexView():
     @pytest.mark.django_db
     def test_index_user_logged_in(self):
         """Tests if a user logged-in can access index view with rigth content"""
-        pass
-        #initial version
-        #test_user_A = User.objects.create(
-        #        username = 'test_user_A',
-        #        password = 'test_user_A',
-        #        birth_date = '2000-01-31',
-        #        email = 'user_A@mail.com',
-        #    )
-        #test_user_A.save()
-        #
-        #client_test_user_A = Client()
-        #client_test_user_A.force_login(test_user_A)
-        #
-        #path = reverse('index')
-        #response = client_test_user_A.get(path)
-        #content = response.content.decode()
-        #
-        #assert response.status_code == 200
-        #assertTemplateUsed(response, "sharingofexperience/index.html")
-        #
-        #assert content.find("<h1>Welcome !</h1>") != -1
-        #assert content.find(">Home</a></button>") != -1
-        #assert content.find("Log out</a>") != -1
+
+        # initial version
+        test_user_A = User.objects.create(
+                username = 'test_user_A',
+                password = 'test_user_A',
+                birth_date = '2000-01-31',
+                email = 'user_A@mail.com',
+            )
+        test_user_A.save()
+        
+        client_test_user_A = Client()
+        client_test_user_A.force_login(test_user_A)
+        
+        path = reverse('index')
+        response = client_test_user_A.get(path)
+        content = response.content.decode()
+        
+        assert response.status_code == 200
+        assertTemplateUsed(response, "sharingofexperience/index.html")
+        assert content.find("<h1>Welcome !</h1>") != -1
+        assert content.find(">Home</a></button>") != -1
+        assert content.find("Log out</a>") != -1
 
 
 class TestHomeView:
 
     @pytest.mark.django_db
     def test_home_user_has_already_access_to_sharings(self):
-        """Tests if a user logged in + who has already access to sharings of experiences -> can access home with rigth content
-        Scenario : 
-        User A and User B log in the application
+        """Tests if a user logged in + who has already access to sharings of experiences -> can access home with rigth content and its dictionnary is not modified
+        Note : We consider that User A has already access to sharings as its profile_model dictionary is
+        {'credits': 1,} instead of {"dictionary initialisation": 1}
         
-        TO BE COMPLETED
-
-        User A access the sharings of experience of user B
-        User B creates sharings of experience
+        Scenario : 
+        User A and User B 
+        User A has a profile model with credits (so we can consider it has already access to sharings as its dictionary does not contain "dictionary initialisation")
+        User B has shared an experience
+        User A logs-in the application and makes a GET request towards Home page.
         """
 
+        # Users creation and connection 
         test_user_A = User.objects.create(
                 username = 'test_user_A',
                 password = 'test_user_A',
@@ -198,22 +202,24 @@ class TestHomeView:
             )
         test_user_B.save()
 
+        # Profile models creation 
         test_user_A_ProfileModelSharingOfExperiencesUserHasAccess = ProfileModelSharingOfExperiencesUserHasAccess.objects.create(
             user = test_user_A,
             sharing_of_experiences_user_has_access = {'credits': 1,},
         )
         test_user_A_ProfileModelSharingOfExperiencesUserHasAccess.save()
-        #print(test_user_A_ProfileModelSharingOfExperiencesUserHasAccess.sharin)
 
-        test_sharing_a = SharingOfExperience.objects.create(
+        # Sharings of experience creation 
+        test_sharing_user_B = SharingOfExperience.objects.create(
                 user_id = test_user_B,
                 experienced_age = 11,
-                description = "description test_sharing_a",
+                description = "description test_sharing",
                 moderator_validation = "NOP",
                 likes = {"likes": {}}
         )
-        test_sharing_a.save()
+        test_sharing_user_B.save()
 
+        # User A makes a GET request towards home
         path = reverse('home')
         response = client_test_user_A.get(path)
         content = response.content.decode()
@@ -223,9 +229,27 @@ class TestHomeView:
         assert content.find(">Sharing experiences</a></button>") != -1 
         assert content.find(">Learing from other people</a></button>") != -1 
 
+        # Test that user A dictionnary is unmodified
+        expected_value = {'credits': 1,}
+        assert test_user_A_ProfileModelSharingOfExperiencesUserHasAccess.sharing_of_experiences_user_has_access == expected_value
+
+
     @pytest.mark.django_db
     def test_home_user_has_not_access_to_sharings_yet(self):
+        """Tests if a user logged in + who has already access to sharings of experiences -> can access home with rigth content and its dictionnary updated
+        Note : We consider that User A has NOT already access to sharings as its profile_model dictionary is {"dictionary initialisation": 1}
+        Note : the ages of user A and shared experience of user B impacts the test : if too much difference: then, 5 credits are allowed; 
+        if enough close : then, 4 credits + a new key in the dictionnary
 
+        Scenario : 
+        User A (age) and User B 
+        User A has a profile model with "dictionary initialisation" --> 
+        User B shared two experiences : the first one corresponds to userA age, the second one is out of the range age_plus_minus (initially gap = 1 year) 
+        User A logs-in the application and makes a GET request towards Home page.
+        User A should have a dictionnary updated with credits instead of {"dictionary initialisation": 1}
+        """
+
+        # Users creation and connection 
         test_user_A = User.objects.create(
                 username = 'test_user_A',
                 password = 'test_user_A',
@@ -244,21 +268,36 @@ class TestHomeView:
             )
         test_user_B.save()
 
+        # Profile models creation 
         test_user_A_ProfileModelSharingOfExperiencesUserHasAccess = ProfileModelSharingOfExperiencesUserHasAccess.objects.create(
             user = test_user_A,
             sharing_of_experiences_user_has_access = {'dictionary initialisation': 1,},
         )
         test_user_A_ProfileModelSharingOfExperiencesUserHasAccess.save()
 
-        test_sharing_a = SharingOfExperience.objects.create(
+        # Sharings of experience creation 
+        test_user_A_birthdate = datetime.strptime(test_user_A.birth_date, "%Y-%m-%d")
+        test_user_A_age = age_calculation(test_user_A_birthdate)
+
+        test_sharing_user_B_1 = SharingOfExperience.objects.create(
                 user_id = test_user_B,
-                experienced_age = 11,
-                description = "description test_sharing_a",
+                experienced_age = test_user_A_age,
+                description = "description test_sharing",
                 moderator_validation = "NOP",
                 likes = {"likes": {}}
         )
-        test_sharing_a.save()
+        test_sharing_user_B_1.save()
 
+        test_sharing_user_B_2 = SharingOfExperience.objects.create(
+                user_id = test_user_B,
+                experienced_age = test_user_A_age+GAP_OF_YEARS_FROM_USER_AGE_FOR_DISPLAYING_EXPERIENCES+1,
+                description = "description test_sharing",
+                moderator_validation = "NOP",
+                likes = {"likes": {}}
+        )
+        test_sharing_user_B_2.save()
+
+        # User A makes a GET request towards home
         path = reverse('home')
         response = client_test_user_A.get(path)
         content = response.content.decode()
@@ -268,24 +307,37 @@ class TestHomeView:
         assert content.find(">Sharing experiences</a></button>") != -1 
         assert content.find(">Learing from other people</a></button>") != -1 
 
-        # test_user_A_ProfileModelSharingOfExperiencesUserHasAccess.sharing_of_experiences_user_has_access --> pas la nouvelle valeur du dico
-        # pour avoir la bonne valeur il faut rechercher le dictionnaire à nouveau
-        user_profile_model = ProfileModelSharingOfExperiencesUserHasAccess.objects.get(user__pk=test_user_A.id)
-        user_profile_model_dictionnary = user_profile_model.sharing_of_experiences_user_has_access
-        print("a", user_profile_model_dictionnary)
-        #assert user_profile_model_dictionnary == {'credits': 5}
+        # Test that user A dictionnary is updated
+        # expected_value = {'credits': ACCESS_TO_SHARINGS_MINIMUM_NUMBER}  # --> as a credit is allowed to a sharing
+        expected_value = {str(test_sharing_user_B_1.id): True, 'credits': ACCESS_TO_SHARINGS_MINIMUM_NUMBER -1}
 
-        #CODE TO TEST
-        #if user_has_not_yet_access_to_sharings_of_experiences(request):  # -->'dictionary initialisation' in profile_model_dictionnary
-        #    user_profile_model_dictionnary_reinitialisation(request)
-        #    sharings_of_experience_age_plus_minus_one = queryset_sharing_of_experiences_from_others(request)
-        #    allocation_of_new_sharings_of_experiences(request, ACCESS_TO_SHARINGS_MINIMUM_NUMBER, sharings_of_experience_age_plus_minus_one)
-        #    # print(timeit.timeit(code_test, number=10000))
-        #return render(request, 'sharingofexperience/home.html')
+        # does not work : print(test_user_A_ProfileModelSharingOfExperiencesUserHasAccess.sharing_of_experiences_user_has_access)
+        # >>> {'dictionary initialisation': 1}
+        user_A_profile_model = ProfileModelSharingOfExperiencesUserHasAccess.objects.get(user__pk=test_user_A.id)
+        user_A_profile_model_dictionnary = user_A_profile_model.sharing_of_experiences_user_has_access
+        # print(user_profile_model_dictionnary)
+        # >>> {'credits': 5} or {2: True, 'credits': 4} ;  depending on ages
 
+        assert user_A_profile_model_dictionnary == expected_value
 
-
-
+    @pytest.mark.django_db
     def test_home_user_not_logged_in(self):
-        pass
+        """Tests if a user not logged in -> can access home with rigth content"""
+
+        # Users creation and connection 
+        test_user_A = User.objects.create(
+                username = 'test_user_A',
+                password = 'test_user_A',
+                birth_date = '2000-01-31',
+                email = 'user_A@mail.com',
+            )
+        test_user_A.save()
+        client_test_user_A = Client()
+        # user does not not log in : client_test_user_A.force_login(test_user_A)
+
+        # User A makes a GET request towards home
+        path = reverse('home')
+        response = client_test_user_A.get(path)
+        assert response.status_code == 302
+        assert response.url == '/login/?next=/home/'
 
