f5262c3ace01c0dd6c48f7052f3b37c69d8d1152
Thomas Creusot <th.creusot@gmail.com>
2023-05-11 15:58:29 +0200

    working on connexion between web app and heroku postgresql database

--- Partdage/settings.py
+++ Partdage/settings.py
@@ -153,7 +153,7 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
         'USER': os.environ.get('DATABASE_USER', 'postgres'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DATABASE_HOST', 'db'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db_test'),
         'PORT': 5432,
     }
 }
