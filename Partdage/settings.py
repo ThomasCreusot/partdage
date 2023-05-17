ae555389f6d11fececb542276f43914cc9dc2308
Thomas Creusot <th.creusot@gmail.com>
2023-05-17 14:40:39 +0200

    working on .circleci/config.yml and settings.py DATABASES

--- Partdage/settings.py
+++ Partdage/settings.py
@@ -165,7 +165,7 @@ DATABASES = {
 """
 
 
-
+"""
 #TEST 18052023
 DATABASES = {
     'default': {
@@ -174,8 +174,24 @@ DATABASES = {
         'DATABASE_URL': os.environ.get('DATABASE_URL', 'db'),
     }
 }
+if os.environ.get('ON_HEROKU') == True:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
+"""
 
+#TEST 18052023 -v 2
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'postgres'),
+        'USER': os.environ.get('DATABASE_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'postgres'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
+        'PORT': 5432,
+    }
+}
+if os.environ.get('ON_HEROKU') == True:
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
