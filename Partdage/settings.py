79299b0fd4083f128af118110eb3c9b3c9788218
Thomas Creusot <th.creusot@gmail.com>
2023-05-17 15:04:16 +0200

    working on .circleci/config.yml and settings.py DATABASES

--- Partdage/settings.py
+++ Partdage/settings.py
@@ -146,7 +146,7 @@ WSGI_APPLICATION = 'Partdage.wsgi.application'
 #    }
 # }
 
-
+"""
 # Try fusion of both DATABASES previous setup
 # PUT DATABASE_HOST=127.0.0.1 in .env file !
 DATABASES = {
@@ -161,11 +161,11 @@ DATABASES = {
 }
 
 # https://devcenter.heroku.com/articles/connecting-heroku-postgres#connecting-in-python
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
-
-
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 """
+
+
+
 #TEST 18052023
 DATABASES = {
     'default': {
@@ -177,7 +177,7 @@ DATABASES = {
 if os.environ.get('ON_HEROKU') == True:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-"""
+
 """
 #TEST 18052023 -v 2
 DATABASES = {
